6f9e8ddf593e408f697ba98884ee10a966422501
Jose Luis  Giraldo <91024863+kronBELET@users.noreply.github.com>
2023-05-10 19:51:30 -0500

    Add files via upload

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -16,23 +16,25 @@ $result = mysqli_query($conn, $sql);
 
 // Comprobar si se han encontrado resultados
 if (mysqli_num_rows($result) > 0) {
-   // Iniciar tabla HTML y encabezados de tabla
-   
-    // Recorrer el conjunto de resultados y mostrar cada fila como una fila de tabla
-    while($row = mysqli_fetch_assoc($result)) {
-        $c_id = $row['c_id'];
-
-        $show .= '<div class="card">
-        <div class="card-content">
-            <h2>'.$row['course_name'].'</h2>
-            <p>'.substr($row['course_description'], 0, 100) . "...".'</p>
-            <a href="course.php?c_id='.$c_id.'" class="button">Detalles</a>
-            <a href="delete_course.php?c_id='.$c_id.'" class="eliminar">Eliminar</a>
-        </div>
-    </div>';
-    }
+    // Iniciar tabla HTML y encabezados de tabla
     
-} else {
+     // Recorrer el conjunto de resultados y mostrar cada fila como una fila de tabla
+     while($row = mysqli_fetch_assoc($result)) {
+         $c_id = $row['c_id'];
+ 
+         $show .= '<div class="card">
+         <div class="card-content">
+             <h2>'.$row['course_name'].'</h2>
+             <p>'.substr($row['course_description'], 0, 100) . "...".'</p>
+             <a href="course.php?c_id='.$c_id.'" class="button">Detalles</a>
+             <a href="delete_course.php?c_id='.$c_id.'" class="eliminar">Eliminar</a>
+ 
+         </div>
+     </div>';
+     }
+     
+ }
+ else {
    // No se han encontrado resultados
     $show = "<div class='error'>No se encontraron cursos.</div>";
 }
--- delete_course.php
+++ delete_course.php
@@ -1,12 +1,23 @@
 <?php
-// Obtener el identificador del curso desde el parámetro 'id'
+
 
 
 // Conectar a la base de datos
 require_once('db_conn.php');
-$c_id=$_get['c_id'];
-    mysqli_query($conexion,"DELETE FROM courses where c_id='$c_id'")or die("error al eliminar");
 
-    mysqli_close($conexion);
 
-    header("location:admin_dashboard");
+// Verifica si se ha pasado el parámetro "id" en la URL
+if (isset($_GET['c_id'])) {
+  // Obtiene el ID del registro a eliminar
+  $id = $_GET['c_id'];
+
+  // Crea la consulta SQL para eliminar el registro
+  $consulta = "DELETE FROM courses WHERE c_id = '$id'";
+
+  // Ejecuta la consulta
+  mysqli_query($conn, $consulta);
+
+  // Redirige a la página principal
+  header('Location: admin_dashboard.php');
+}
+?>
