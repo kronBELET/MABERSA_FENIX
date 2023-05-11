c1512f3e10e9f9e4e728a44174c78d6d634c190c
Jose Luis  Giraldo <91024863+kronBELET@users.noreply.github.com>
2023-05-10 20:43:39 -0500

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+// Conectar a la base de datos
+require_once('db_conn.php');
+
+
+// CURSOS
+if (isset($_GET['c_id'])) {
+  // Obtiene el ID del registro a eliminar
+  $id = $_GET['c_id'];
+
+  // Crea la consulta SQL para eliminar el registro
+  $consulta = "DELETE FROM courses WHERE c_id = '$id'";
+
+  // Ejecuta la consulta
+  mysqli_query($conn, $consulta);
+
+  // Redirige a la página principal
+  header('Location: admin_dashboard.php');
+}
+
+// USUARIOS
+if (isset($_GET['u_id'])) {
+  // Obtiene el ID del registro a eliminar
+  $id = $_GET['u_id'];
+
+  // Crea la consulta SQL para eliminar el registro
+  $consulta = "DELETE FROM users WHERE u_id = '$id'";
+
+  // Ejecuta la consulta
+  mysqli_query($conn, $consulta);
+
+  // Redirige a la página principal
+  header('Location: user.php');
+}
+?>
